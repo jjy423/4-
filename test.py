a38e19f62882b97c698b0b5d862d859685995695
jjy423 <79985009+jjy423@users.noreply.github.com>
2021-05-09 23:55:00 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,86 @@
+# 백준 알고리즘 2557번
+# print("Hello Warld")
+
+# 백준 알고리즘 10718번
+# print("강한친구 대한육군\n강한친구 대한육군")
+
+# 백준 알고리즘 1000번
+# a,b = input().split()
+# a = int(a)
+# b = int(b)
+# print(a+b)
+
+# 백준 알고리즘 1001번 
+# a,b = input().split()
+# a = int(a)
+# b = int(b)
+# print(a-b)
+
+# 백준 알고리즘 10998번
+# a,b = map(int, input().split())
+# a = int(a)
+# b = int(b)
+# print(a*b)
+
+# map 함수.
+# 정의: 여러 개의 데이터를 한 번에 다른 형태로 변환하기 위해 사용한다.
+#       (주로 list,tuple을 대상으로 사용하는 함수.)
+# 사용방법: map(변환 함수, 순회 가능한 데이터)
+
+# 백준 알고리즘 1008번
+# a,b = map(float, input().split())
+# a = int(a)
+# b = int(b)
+# c = a/b
+# print('%.9f'%c)
+# → %.9f 는 소수점 9번째 자리까지 출력한다는 의미이다.
+
+# float 함수
+# 정의 1.: 실수를 표현하는 방식인 부동소주점(floating point) 방식.
+# 정의 2 : 소수점이 떠다니며 움직인다는 의미의 부동이다. 
+# 하는일: 내장함수 float()를 이용해서 숫자나 문자열을 실수형(Float)으로 변환할 수 있다.
+
+# 백준 알고리즘 10869번
+# a,b = map(int, input().split())
+# a = int(a)
+# b = int(b)
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a//b)
+# print(a%b)
+
+# 백준 알고리즘 10430번
+# a,b,c = map(int, input().split())
+# a = int(a)
+# b = int(b)
+# c = int(c)
+# print((a+b)%c)
+# print((a%c + b%c)%c)
+# print((a*b)%c)
+# print((a%c)*(b%c)%c)
+
+# 백준 알고리즘 2588번
+
+# c = a*5
+# d = a*8
+# e = a*3
+# f = a*b
+# print(c)
+# print(d)
+# print(e)
+# print(f)
+
+# a = int(input())
+# b = int(input())
+
+# c = int(b / 100)
+# d = int((b % 100) / 10)
+# e = int(b % 10)
+
+# print(a*e)
+# print(a*d)
+# print(a*c)
+# print(a*b)
+
+# if a>=0 and a<=100 and if b>=0 and b<=100:
